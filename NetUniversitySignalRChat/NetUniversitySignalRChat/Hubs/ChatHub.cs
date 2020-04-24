4415761b458932de001425d412edcc4f3f9c6f46
Miguel teheran <miguelteheran@gmail.com>
2020-04-23 21:29:22 -0500

    se agrega hub y proyecto base

---
+++ NetUniversitySignalRChat/NetUniversitySignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace NetUniversitySignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task EnviarMensaje(string usuario, string mensaje)
+        {
+            await Clients.All.SendAsync("RecibirMensaje", usuario, mensaje);
+        }
+    }
+}
