1e4f9c6ad1f5256e1659c4cd2a1136043a3066dd
Miguel teheran <miguelteheran@gmail.com>
2020-04-26 14:27:47 -0500

    se agrega aplicacion de consola

---
+++ NetUniversitySignalRChat/NetUniversitySignalRChatConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+
+namespace NetUniversitySignalRChatConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.Thread.Sleep(2000);
+
+            var conexion = new HubConnectionBuilder().WithUrl("http://localhost:50380/chatHub").Build();
+
+            conexion.On<Mensaje>("RecibirMensaje", (mensaje) =>
+            {
+                Console.WriteLine($"{mensaje.Usuario} - {mensaje.Contenido}");
+            });
+
+            conexion.StartAsync().Wait();
+
+            while (true)
+            {
+                var contenidoMensaje = Console.ReadLine();
+
+                conexion.InvokeAsync("EnviarMensaje", new Mensaje() { Usuario = "Consola", Contenido = contenidoMensaje });
+            }
+        }
+    }
+}
